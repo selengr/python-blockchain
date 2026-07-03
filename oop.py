ef55b93856d80ef1d30e82ba594160ce63745ff5
selengr <reza1997karbakhsh@gmail.com>
2026-07-03 21:32:44 +0330

    create lib

--- oop.py
+++ oop.py
@@ -1,18 +1,30 @@
-entered_number = int(input("Enter a number please: "))
+class Animal():
+    def __init__(self, name, species, age, sound):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.sound = sound
+        
+        
+    def make_sound(self):
+        return f"im a lion {self.sound}"
+    
+    def zoo_name(self): 
+        return "welcome to kerman zoo live"
 
-num = 2
+    def info(self): 
+        if(self.name == "lion"):
+            return f"i am the strongest animal of all the world that set iam a {self.name}"
+    
+    def __str__(self):
+        return f"you know who is the strongest animal of all the world meeeeee"
+    
+    
+animal = Animal("lion", "german", 5, "bark bark")
+print(animal)
+print(animal.name)
+print(animal.sound)
 
-while num <= entered_number:
-    i = 2
-    is_aval = True
-
-    while i < num:
-        if num % i == 0:
-            is_aval = False
-            break
-        i += 1
-
-    if is_aval:
-        print(num)
-
-    num += 1
+# print(animal.make_sound())
+# print(animal.zoo_name())
+print(animal.info())
