2a7ca9a97d09fca50de76a146f2acb0b4491939b
selengr <reza1997karbakhsh@gmail.com>
2026-09-07 01:42:14 +0330

    Expose blockchain package exports for clean imports.

---
+++ blockchain/__init__.py
@@ -0,0 +1,11 @@
+from blockchain.block import Block
+from blockchain.chain import Blockchain
+from blockchain.network import Network
+from blockchain.transaction import Transaction
+
+__all__ = [
+    "Transaction",
+    "Block",
+    "Blockchain",
+    "Network",
+]
