c59c45d3e2b30d32615ee975b52c7afd86aac37c
selengr <reza1997karbakhsh@gmail.com>
2026-09-07 23:26:08 +0330

    Export Wallet from the blockchain package.

--- blockchain/__init__.py
+++ blockchain/__init__.py
@@ -2,10 +2,12 @@ from blockchain.block import Block
 from blockchain.chain import Blockchain
 from blockchain.network import Network
 from blockchain.transaction import Transaction
+from blockchain.wallet import Wallet
 
 __all__ = [
     "Transaction",
     "Block",
     "Blockchain",
     "Network",
+    "Wallet",
 ]
